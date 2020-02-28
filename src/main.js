4daa1800825b193cb3aa1898a41fb0bc76c93df4
Symbolk <symbolk@163.com>
2020-02-28 23:12:26 +0800

    [ui] Rename the project.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Icon from 'vue-awesome/components/Icon'
+
+Vue.component('v-icon', Icon)
+Vue.use(BootstrapVue)
+Vue.use(VueSidebarMenu)
 
 Vue.config.productionTip = false
 
